6da131911517a8866efead38eb5cc325ccddf78a
Dharshu2026 <dharshdharsh1020@gmail.com>
2026-02-04 23:08:07 +0530

    Updated webDir to public/build for Vite

--- public/public/sw.js
+++ public/public/sw.js
@@ -1,19 +1,19 @@
-// public/sw.js
-const cacheName = 'pk-agency-v1';
-const assets = ['/', '/css/app.css', '/js/app.js'];
+// // public/sw.js
+// const cacheName = 'pk-agency-v1';
+// const assets = ['/', '/css/app.css', '/js/app.js'];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(cacheName).then(cache => {
-            return cache.addAll(assets);
-        })
-    );
-});
+// self.addEventListener('install', event => {
+//     event.waitUntil(
+//         caches.open(cacheName).then(cache => {
+//             return cache.addAll(assets);
+//         })
+//     );
+// });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//         caches.match(event.request).then(response => {
+//             return response || fetch(event.request);
+//         })
+//     );
+// });
